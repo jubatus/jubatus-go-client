d113af39db276411c87d3578bb3e6b66b232d9dd
KUMAZAKI Hiroki <kumazaki.hiroki@lab.ntt.co.jp>
2015-01-09 19:44:43 +0900

    initial commit

---
+++ common/datum.go
@@ -0,0 +1,67 @@
+package jubatus_client
+
+import (
+)
+
+const (
+	string_values = iota
+	num_values
+	binary_values
+)
+
+type Datum [3][]interface{}
+
+func (d *Datum) String() string {
+	ret := ""
+	ret += "[{string_values: ["
+	return ret
+}
+
+func NewDatum() Datum {
+	return Datum{}
+}
+
+// string
+
+func (d *Datum) StringValues() []interface{} {
+	return d[string_values]
+}
+
+func (d *Datum) AddString(key string, value string) *Datum {
+	if d[string_values] == nil {
+		d[string_values] = make([]interface{}, 0)
+	}
+	d[string_values] = append(d[string_values],
+		                               [2]interface{}{key, value})
+	return d
+}
+
+// num
+
+func (d *Datum) NumValues() []interface{} {
+	return d[num_values]
+}
+
+func (d *Datum) AddNum(key string, value float64) *Datum {
+	if d[num_values] == nil {
+		d[num_values] = make([]interface{}, 0)
+	}
+	d[num_values] = append(d[num_values],
+                         		   [2]interface{}{key, value})
+	return d
+}
+
+// binary
+
+func (d *Datum) BinaryValues() []interface{} {
+	return d[binary_values]
+}
+
+func (d *Datum) AddBinary(key string, value []byte) *Datum {
+	if d[binary_values] == nil {
+		d[binary_values] = make([]interface{}, 0)
+	}
+	d[binary_values] = append(d[binary_values],
+                         		   [2]interface{}{key, value})
+	return d
+}
---
+++ util/jubatus_visor.go
@@ -0,0 +1,104 @@
+package util
+
+import (
+	"os/exec"
+	"fmt"
+	"strings"
+	//	"bytes"
+	"os"
+	"bufio"
+)
+
+type JubatusProcess struct {
+	cmd *exec.Cmd
+	Port int
+}
+
+func jubatus_keep(target exec.Cmd) {
+	for {
+		target.Wait()
+		target.Start()
+	}
+}
+
+type JubatusProcessError struct {
+	message string
+}
+func (j *JubatusProcessError) Error() string {
+	return fmt.Sprintf("Jubatus Process Error: %s", j.message)
+}
+
+func (j *JubatusProcess) Kill() error {
+	if err := j.cmd.Process.Kill(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewJubatusProcess(command string, filepath string) (*JubatusProcess, error) {
+	port := 9200
+	for {
+		cmd := exec.Command(command, "-f", filepath, "-p", fmt.Sprintf("%d", port))
+    cmd.Stderr = os.Stderr
+
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			return nil, err
+		}
+
+		if err := cmd.Start(); err != nil {
+			return nil, err
+		}
+
+		r := bufio.NewReader(stdout)
+		for {
+			byteline, _, err := r.ReadLine()
+			if err != nil {
+				return nil, err
+			}
+			line := string(byteline)
+			if strings.Contains(line, "RPC server startup") {
+				return &JubatusProcess{cmd, port}, nil
+			} else if strings.Contains(line, "server failed to start") {
+				port += 1
+				break
+			}
+		}
+	}
+}
+
+
+/*
+func main() {
+	proc, err := NewJubatusProcess("hoge")
+	if err != nil {
+		panic(err)
+	}
+	defer proc.Kill()
+	client, err := proc.GetClient()
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 10; i += 1 {
+		go func() {
+			client, err := proc.GetClient()
+			if err != nil {
+				panic(err)
+			}
+
+			for {
+				d := jubatus.NewDatum()
+				d.AddNum(fmt.Sprintf("f-%d", i), float64(i))
+				client.SimilarRowFromDatum(d, 10)
+			}
+		}()
+	}
+	for cnt := 0; cnt < 10; cnt += 1{
+		d := jubatus.NewDatum()
+		d.AddNum(fmt.Sprintf("f-%d", cnt), float64(cnt))
+		client.UpdateRow(fmt.Sprintf("row-%d", cnt), d)
+	}
+	time.Sleep(10 * 1000 * 1000 * 1000)
+}
+*/
