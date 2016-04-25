779c069c2eda50ff8febcb02070adc7f876644e6
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2016-04-25 16:47:06 +0900

    bump version

--- lib/burst/types.go
+++ lib/burst/types.go
@@ -1,4 +1,4 @@
-// This file is auto-generated from burst.idl(0.6.4-96-g66ed74d) with jenerator version 0.7.2-85-g1b6087f/fix-go-client
+// This file is auto-generated from burst.idl(0.6.4-96-g66ed74d) with jenerator version 0.8.5-6-g5a2c923/master
 // *** DO NOT EDIT ***
 
 package jubatus_client
--- lib/classifier/types.go
+++ lib/classifier/types.go
@@ -1,4 +1,4 @@
-// This file is auto-generated from classifier.idl(0.7.2-49-g5a6436d) with jenerator version 0.7.2-85-g1b6087f/fix-go-client
+// This file is auto-generated from classifier.idl(0.8.9-17-gd4c007f) with jenerator version 0.8.5-6-g5a2c923/master
 // *** DO NOT EDIT ***
 
 package jubatus_client
