e89a673c2c5236b4d7862967ad66d71e9e77460c
IMAMASU Ryohei <imms.ryohei@gmail.com>
2017-10-30 15:21:56 +0900

    bump version

--- lib/classifier/types.go
+++ lib/classifier/types.go
@@ -1,4 +1,4 @@
-// This file is auto-generated from classifier.idl(0.8.9-17-gd4c007f) with jenerator version 0.9.4-42-g70f7539/master
+// This file is auto-generated from classifier.idl(0.8.9-17-gd4c007f) with jenerator version 0.9.4-42-g70f7539/develop
 // *** DO NOT EDIT ***
 
 package jubatus_client
