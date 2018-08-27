4e3fd6a49aa9bf7f3d78bf023c909bf30ab0fff7
IMAMASU Ryohei <imms.ryohei@gmail.com>
2018-08-27 15:57:32 +0900

    bump version

--- lib/weight/types.go
+++ lib/weight/types.go
@@ -1,4 +1,4 @@
-// This file is auto-generated from weight.idl(0.9.0-24-gda61383) with jenerator version 1.0.7-6-g1ae743a/master
+// This file is auto-generated from weight.idl(1.0.9-6-gd5557e3) with jenerator version 1.0.7-6-g1ae743a/master
 // *** DO NOT EDIT ***
 
 package jubatus_client
